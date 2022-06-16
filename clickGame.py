33ed43118ea0b1d8a90576a0aa187313a66f19e3
San Lazar's Knigth ✠ <33008163+Lucoberto@users.noreply.github.com>
2022-06-16 23:31:39 +0200

    Upload main game

---
+++ clickGame.py
@@ -0,0 +1,81 @@
+from logging import root
+from re import X
+from tkinter import *
+import tkinter as tk
+from tkinter import Button, ttk
+from turtle import exitonclick, title
+
+point = 0
+
+# The save file is game.sv onli save the score
+
+# Menu to see the score
+def bestScore():
+    pass
+
+def clean():
+    global point
+    point = 0
+
+# Saving game points
+def saveGame(): 
+    global point
+    saving = open('game.sv', 'w')
+    saving.write(str(point))
+    saving.close()
+
+# Loading game points
+def load():
+    global point
+    reading = open('game.sv', 'r')
+    point_read = reading.read()
+    point = int(point_read)
+    reading.close
+
+# Window loop
+
+root = tk.Tk()
+
+# Point generator logic
+def points():
+    global point
+    point += 1
+    label_info.config(text=f"Your points {point}")
+
+
+root.title("Click Game")
+root.config(height=300,width=200)
+
+label_info = ttk.Label(text="Your points 0")
+label_info.place(x=20, y=10)
+
+confirm = ttk.Button(text="Click", command=points)
+confirm.place(x=60, y=40)
+
+# The top menu in there can load, save and exit to the game
+
+loadMenu = Menu(root)
+topMenu = Menu(loadMenu, tearoff=0)
+
+#-----------------Settings menu---------------------#
+
+loadMenu.add_cascade(label="Settings", menu=topMenu)
+topMenu.add_command(label="Save", command=saveGame)
+topMenu.add_command(label="Load", command=load)
+topMenu.add_command(label="Clean", command=clean)
+
+#-----------------Cheat menu----------------------#
+
+cheatsMenu = Menu(loadMenu, tearoff=0)
+loadMenu.add_cascade(label="Cheats", menu=cheatsMenu)
+cheatsMenu.add_command(label="x2")
+cheatsMenu.add_command(label="Coins")
+cheatsMenu.add_command(label="Score")
+
+#-----------------Exit button----------------------#
+
+loadMenu.add_command(label="Exit", command=exit)
+
+root.config(menu=loadMenu)
+
+root.mainloop()
